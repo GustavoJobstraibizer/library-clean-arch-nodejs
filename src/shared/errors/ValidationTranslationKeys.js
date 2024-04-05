bb1981d616a8910cf7cf6c679fcfaf6ddb9c4793
Gustavo Jobstraibizer <gustavojobs_dev@outlook.com>
2024-04-05 18:06:28 -0300

    feat: adiciona classe para manter as chaves de erros utilizadas

---
+++ src/shared/errors/ValidationTranslationKeys.js
@@ -0,0 +1,11 @@
+module.exports = class ValidationTranslationKeys {
+  // Generic
+  static REQUIRED_FIELD_ERROR = 'requiredFieldError';
+  static INVALID_DATE = 'invalidDate';
+  static DEPENDENCY_ERROR = 'dependencyError';
+  static NOT_FOUND = 'notFound';
+
+  // EmprestimoLivro Rules
+  static RETURN_DATE_BEFORE_START_DATE = 'returnDateBeforeStartDate';
+  static BOOK_ALREADY_LENT = 'bookAlreadyLent';
+};
