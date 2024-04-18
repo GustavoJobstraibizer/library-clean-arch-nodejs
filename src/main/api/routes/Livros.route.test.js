5d5bcc18ea51fafb1cf15bfe814ab5c36956184a
Gustavo Jobstraibizer <gustavojobs_dev@outlook.com>
2024-04-18 17:17:51 -0300

    test: Livros route

--- src/main/api/routes/Livros.route.test.js
+++ src/main/api/routes/Livros.route.test.js
@@ -48,11 +48,47 @@ describe('Routes: Livros', () => {
 
     await typeOrmLivroRepository.save(livroDTO);
 
-    const { statusCode, body } = await request(app).get(
-      `/livros?valor=${livroDTO.ISBN}`,
-    );
+    const { statusCode, body } = await request(app)
+      .get(`/livros`)
+      .query({ valor: livroDTO.ISBN });
     expect(body[0].id).toBeDefined();
     expect(statusCode).toBe(200);
     expect(body[0]).toEqual(expect.objectContaining(livroDTO));
+  });
+
+  it('deve retornar uma lista de livros ao buscar por nome', async () => {
+    const livroDTO = {
+      nome: 'Livro Teste',
+      quantidade: 10,
+      autor: 'Autor Teste',
+      genero: 'Genero Teste',
+      ISBN: '1234567890',
+    };
+
+    await typeOrmLivroRepository.save(livroDTO);
+
+    const { statusCode, body } = await request(app)
+      .get(`/livros`)
+      .query({ valor: livroDTO.nome });
+    expect(body[0].id).toBeDefined();
+    expect(statusCode).toBe(200);
+    expect(body[0]).toEqual(expect.objectContaining(livroDTO));
+  });
+
+  it('deve retornar uma lista vazia ao buscar por nome ou ISBN', async () => {
+    const { statusCode, body } = await request(app)
+      .get(`/livros`)
+      .query({ valor: 'Livro Teste' });
+
+    expect(statusCode).toBe(200);
+    expect(body).toEqual([]);
+  });
+
+  it('deve retornar erro quando o campo valor não for fornecido', async () => {
+    const { statusCode, body } = await request(app).get(`/livros`).query({});
+
+    expect(statusCode).toBe(400);
+    expect(body.errors).toBeDefined();
+    expect(body.errors.fieldErrors).toEqual({ valor: ['Valor é obrigatório'] });
   });
 });
